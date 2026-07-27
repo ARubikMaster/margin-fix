9c8040b8cca41f2c46de9a259a66465ecd030d00
MrGreg <epiccooldog@techie.com>
2026-07-27 17:12:01 -0400

    JS fix

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 let unpatch;
 
-export default {
+module.exports = {
     onLoad: () => {
-        // Access the global API injected by Revenge/Vendetta
         const { metro, patcher } = window.vendetta;
-        
-        // Find the server list component
         const GuildListView = metro.findByName("GuildListView", false);
         
         if (GuildListView) {
-            // Inject the margin right after the component renders
             unpatch = patcher.after("default", GuildListView, (args, res) => {
                 if (res && res.props) {
                     res.props.style = [res.props.style, { marginLeft: 25 }];
