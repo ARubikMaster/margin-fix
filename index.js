0e17bfc4e72d191502a0b6f1dce303ec002fa8c2
MrGreg <epiccooldog@techie.com>
2026-07-27 17:26:41 -0400

    JS fix 2

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-let unpatch;
+(function (exports, metro, patcher) {
+    "use strict";
 
-module.exports = {
-    onLoad: () => {
-        const { metro, patcher } = window.vendetta;
-        const GuildListView = metro.findByName("GuildListView", false);
+    let unpatch;
+
+    const MarginFix = {
+        onLoad: () => {
+            // Find the server list component
+            const GuildListView = metro.findByName("GuildListView", false);
+            
+            if (GuildListView) {
+                // Inject the margin right after the component renders
+                unpatch = patcher.after("default", GuildListView, (args, res) => {
+                    if (res && res.props) {
+                        res.props.style = [res.props.style, { marginLeft: 25 }];
+                    }
+                });
+            }
+        },
         
-        if (GuildListView) {
-            unpatch = patcher.after("default", GuildListView, (args, res) => {
-                if (res && res.props) {
-                    res.props.style = [res.props.style, { marginLeft: 25 }];
-                }
-            });
+        onUnload: () => {
+            if (unpatch) unpatch();
         }
-    },
-    onUnload: () => {
-        if (unpatch) unpatch();
-    }
-}
+    };
+
+    // Tell Revenge this is a valid module
+    exports.default = MarginFix;
+    Object.defineProperty(exports, "__esModule", { value: true });
+    
+    return exports;
+
+})({}, vendetta.metro, vendetta.patcher);
